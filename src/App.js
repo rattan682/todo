a195f1b2d0caa2fb44698823efcac0a99c0610a8
Rattandeep singh <rattandehar@gmail.com>
2024-09-03 23:59:40 +0530

    todo-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [todo,settodo]=useState([])
+  const [editindex,seteditindex]=useState(null)
+  const edittedtext=useRef(null)
+  const newinput=useRef(null)
+  const handledel=(index)=>{
+    const newtodo=todo.filter((e,i)=>i!==index)
+    settodo(newtodo)
+  }
+  const handleadd=()=>{
+    if(newinput.current.value!=""){settodo([...todo,newinput.current.value])
+      newinput.current.value=""
+    }
+  }
+  const handleedit=()=>{
+    todo.splice(editindex,1,edittedtext.current.value)
+    seteditindex(null)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <input ref={newinput} type="text" /><button onClick={()=>handleadd()}>Submit</button>
+  {todo.map((e,index)=><>{editindex!=index?<div>{e}<button onClick={()=>handledel(index)}>delete</button><button onClick={()=>seteditindex(index)}>edit</button></div>:<div><input ref={edittedtext} type="text" placeholder={todo[editindex]}/><button onClick={()=>handleedit()}>save</button></div>}</>)}
+   </>
   );
 }
 
